dc95003aed0f006406ed60a6b3d3dbe62a88a5ee
xavierviguera <xavier.viguera.dev@gmail.com>
2023-11-26 16:33:09 +0100

    Community creation successfull, need to test images

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -8,15 +8,13 @@ class CommunitiesController < ApplicationController
     # POST /communities.json
     def create
         # Creates an instance of community
-        @communtity = Community.new(community_params)
+        @community = Community.new(community_params)
 
-        respond_to do |format|
-            # Save it in DB
-            if @community.save
-                format.json { render json: { message: 'Community created successfully', community: @community }, status: :created }
-            else
-                format.json { render json: { errors: @community.errors.full_messages }, status: :unprocessable_entity }
-            end
+        # Save it in DB
+        if @community.save
+            render json: { message: 'Community created successfully', community: @community }, status: :created
+        else
+            render json: { errors: @community.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
