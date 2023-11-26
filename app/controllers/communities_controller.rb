8c51d7a0e9487b724814242afc7268e2e4469193
xavierviguera <xavier.viguera.dev@gmail.com>
2023-11-26 18:26:47 +0100

    Allow images in communities

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,28 +1,52 @@
 class CommunitiesController < ApplicationController
 
-    # GET /communities.json
+    # GET /communities
     def index
         @communities = Community.all.order(name: :asc)
 
-        render json: { communities: @communities }, status: :ok
+        communities_json = @communities.map do |community|
+            {
+              id: community.id,
+              identifier: community.identifier,
+              name: community.name,
+              community_avatar: community.community_avatar.attached? ? url_for(community.community_avatar) : nil,
+              created_at: community.created_at,
+              updated_at: community.updated_at,
+            }
+        end
+
+        render json: { communities: communities_json }, status: :ok
     end
+
+
     
-    # POST /communities.json
+    # POST /communities
     def create
         # Creates an instance of community
         @community = Community.new(community_params)
 
         # Save it in DB
         if @community.save
-            render json: { message: 'Community created successfully', community: @community }, status: :created
+            render json: {
+              message: 'Community created successfully',
+              community: {
+                id: @community.id,
+                identifier: @community.identifier,
+                name: @community.name,
+                created_at: @community.created_at,
+                updated_at: @community.updated_at
+              }
+            }, status: :created
         else
-            render json: { errors: @community.errors.full_messages }, status: :unprocessable_entity
+            render json: {
+              errors: @community.errors.full_messages
+            }, status: :unprocessable_entity
         end
     end
 
     private
         # Only allow a list of trusted parameters through.
         def community_params
-            params.require(:community).permit(:identifier, :name, :avatar, :banner)
+            params.require(:community).permit(:identifier, :name, :community_avatar, :community_banner)
         end
 end
