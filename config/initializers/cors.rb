384ca8f0f9afb33d7f9c9b56840b94b207e84600
mcampsll <145116519+mcampsll@users.noreply.github.com>
2023-12-22 01:06:16 +0100

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000/'
+      origins 'https://entrega2api.fly.dev', 'http://localhost:3000', 'http://localhost:5173'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put]
     end
   end
